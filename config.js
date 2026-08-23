1f20345ac05b9b2da78c584fb29c09db5b87ca11
Flora Gabriagues <flora.gabriagues@etik.com>
2026-08-23 17:16:22 +0200

    Branche l'API Scaleway sur le site

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // URL de l'API EO_Toolbox (sans slash final).
 // Laisser vide ("") pour désactiver le mode "simulation réelle" : le site
 // retombe alors sur le moteur JS temps réel, et le bouton Simuler disparaît.
-var API_BASE = "";
+var API_BASE = "https://nssharphopper7bba2963-container-kind-mcclintock.functions.fnc.fr-par.scw.cloud";
 
 // Dossier des images de presets pré-calculées par render_presets.py.
 // Si une image manque, le preset retombe silencieusement sur le moteur JS.
