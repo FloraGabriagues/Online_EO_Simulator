08a51fe0f6ae3efb4f9bdc143a803c08be4dc28e
Flora Gabriagues <flora.gabriagues@etik.com>
2026-09-15 20:16:53 +0200

    fix: erreur paiement

--- supabase/functions/create-checkout-session/index.ts
+++ supabase/functions/create-checkout-session/index.ts
@@ -11,11 +11,10 @@
 //   SUPABASE_ANON_KEY
 //   SUPABASE_SERVICE_ROLE_KEY
 
-import Stripe from "https://esm.sh/stripe@14.21.0?target=deno";
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2.45.0";
+import Stripe from "npm:stripe@17.7.0";
+import { createClient } from "npm:@supabase/supabase-js@2";
 
 const stripe = new Stripe(Deno.env.get("STRIPE_SECRET_KEY")!, {
-  apiVersion: "2024-06-20",
   httpClient: Stripe.createFetchHttpClient(),
 });
 
